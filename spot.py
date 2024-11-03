5b7635687e923bdd26e2537ed6b08860c138cd52
Julien Bonte <bonte.julien@gmail.com>
2024-11-03 10:01:00 +0100

    Update scripts and configurations: replace repopack with repomix and clean up .gitignore for clarity on generated files

--- spot.py
+++ spot.py
@@ -1,4 +1,3 @@
-import time
 import signal
 import sys
 
--- watch.sh
+++ watch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Surveillance des changements de fichiers avec entr
-find . -name "*.py" | entr -r bash -c 'npx repopack && ./transfer.sh'
+find . -name "*.py" | entr -r bash -c 'npx repomix && ./transfer.sh'
